085418b21de58505d7ddf8f49112be909efa2713
OkemmaduNancy <68012458+OkemmaduNancy@users.noreply.github.com>
2021-03-16 09:45:31 +0000

    adding the word "even"
    Even

--- even-numbers/script.js
+++ even-numbers/script.js
@@ -1,11 +1,12 @@
 function evenCal() {
     for (let x = 1; x <= 60; x++) {
         if (x == 0) {
-            console.log(x );
+            console.log(x + "even");
         }
         if (x % 2 == 0) {
-            console.log(x );
+            console.log(x + "even");
         }
+        
         
     }
 }
