973217bd5c7afe9473301af161d66117a7a1c2c3
Okemmadu Nancy <anurika.okemmadu@gmail.com>
2021-03-16 10:12:19 +0000

    new file script.js

---
+++ even-numbers/script.js
@@ -0,0 +1,12 @@
+function evenCal() {
+    for (let x = 1; x <= 60; x++) {
+        if (x == 0) {
+            console.log(x );
+        }
+        if (x % 2 == 0) {
+            console.log(x );
+        }
+        
+    }
+}
+const value = evenCal();
